8c883e5aaea6c1b745ab476949b49c5ea091b7dd
Lawalolabode <lawalsulaimon003@gmail.com>
2024-12-07 15:20:33 +0100

    first assignment

---
+++ OneDrive/Documents/java/TaskFive.java
@@ -0,0 +1,21 @@
+public class TaskFive {
+	public static void main(String[] args) {
+
+
+	
+	for(int counter = 1; counter < 10; counter++) {
+	if(counter % 4 == 0){
+	 
+	for(int i = 0; i < 5; i++){
+	 System.out.println(counter); 
+
+}
+	
+}
+}
+	
+  }
+
+
+
+}
