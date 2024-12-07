53b54e7c230e763fb33830571910d34afe05fc9a
Lawalolabode <lawalsulaimon003@gmail.com>
2024-12-07 15:21:37 +0100

    first assignment

---
+++ OneDrive/Documents/java/TaskSix.java
@@ -0,0 +1,30 @@
+public class TaskSix {
+	public static void main(String[] args) {
+
+ int resultFour = 1;
+ 
+ 
+for(int counter =0; counter < 5; counter++){
+
+int four = 4;			
+resultFour *= four;
+		
+ System.out.println(resultFour);}
+	
+int resultEight = 1;
+
+for(int counter =0; counter < 5; counter++){
+
+int eight = 8;
+resultEight *= eight;
+
+System.out.println(resultEight);
+}
+	
+   
+
+}
+
+
+
+}
