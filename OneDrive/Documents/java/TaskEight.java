95b69c050d3aaca782441fb658643468845c900a
Lawalolabode <lawalsulaimon003@gmail.com>
2024-12-07 15:23:51 +0100

    first assignment

---
+++ OneDrive/Documents/java/TaskEight.java
@@ -0,0 +1,33 @@
+public class TaskEight {
+	public static void main(String[] args) {
+
+ int resultFour = 1;
+ int sumFour = 0;
+ int addition = 0
+for(int counter =0; counter < 5; counter++){
+
+int four = 4;			
+resultFour *= four;
+sumFour += resultFour;		
+}
+	
+int resultEight = 1;
+int sumEight = 0;
+
+for(int counter =0; counter < 5; counter++){
+
+int eight = 8;
+resultEight *= eight;
+sumEight += resultEight;
+
+}
+
+addition = sumFour + sumEight;
+
+System.out.println(addition);
+
+}
+
+
+
+}
