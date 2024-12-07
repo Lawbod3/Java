acdfbaa5c7c7c977129d4e361dd98efb3fd0a707
Lawalolabode <lawalsulaimon003@gmail.com>
2024-12-07 15:06:55 +0100

    first assignment

---
+++ OneDrive/Documents/java/TaskOne.java
@@ -0,0 +1,21 @@
+public class TaskOne {
+	public static void main(String[] args) {
+
+
+	
+	int counter = 0;
+
+	while(counter < 10) {
+	 counter++;
+
+	System.out.println(	counter );
+
+}
+	
+    
+
+}
+
+
+
+}
